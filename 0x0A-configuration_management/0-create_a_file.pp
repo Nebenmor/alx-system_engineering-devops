c21f0e3999bcc863eb25a37058113f7b05a67346
nebenmor <nebenmor.anthony@gmail.com>
2024-04-19 09:21:34 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 #This helps creates a file
 file { '/tmp/school':
+	ensure	=>	'file',
 	mode	=>	'0744',
 	owner	=>	'www-data',
 	group	=>	'www-data',
-	content =>	'I love Puppet',
+	content	=>	'I love Puppet',
 }
 
